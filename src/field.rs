5c192ef854f0442ae497417b0aad6257ffec44a1
Streamer272 <daniel.svitan.team7274dev@gmail.com>
2022-02-24 20:55:17 +0100

    :sparkles: Adds print method

---
+++ src/field.rs
@@ -0,0 +1,123 @@
+use std::fmt::{Formatter, Display, Result};
+use terminal_size::{terminal_size};
+
+enum BoxContent {
+    Empty,
+    X,
+    O,
+}
+
+impl Display for BoxContent {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            BoxContent::Empty => write!(f, " "),
+            BoxContent::X => write!(f, "X"),
+            BoxContent::O => write!(f, "O"),
+        }
+    }
+}
+
+pub struct Box {
+    content: BoxContent,
+    selected: bool,
+}
+
+impl Box {
+    pub fn format() -> String {
+        String::from("")
+    }
+}
+
+pub struct Field {
+    boxes: [Box; 9],
+}
+
+pub enum Direction {
+    Left,
+    Down,
+    Up,
+    Right,
+}
+
+impl Field {
+    pub fn new() -> Field {
+        Field {
+            boxes: [
+                Box{content: BoxContent::Empty, selected: false},
+                Box{content: BoxContent::Empty, selected: false},
+                Box{content: BoxContent::Empty, selected: false},
+                Box{content: BoxContent::Empty, selected: false},
+                Box{content: BoxContent::Empty, selected: true},
+                Box{content: BoxContent::Empty, selected: false},
+                Box{content: BoxContent::Empty, selected: false},
+                Box{content: BoxContent::Empty, selected: false},
+                Box{content: BoxContent::Empty, selected: false},
+            ],
+        }
+    }
+
+    pub fn get_box(&self, index: usize) -> &Box {
+        &self.boxes[index]
+    }
+
+    pub fn move_selected(&self, direction: Direction) {
+        let current = self.boxes.iter().position(|b| b.selected).unwrap();
+
+        // match direction {
+        //     Direction::Left => {
+        //         &self.boxes[current - 1].selected = &true;
+        //     },
+        //     Direction::Down => {
+        //         &self.boxes[current + 3].selected = &true;
+        //     },
+        //     Direction::Up => {
+        //         &self.boxes[current - 3].selected = &true;
+        //     },
+        //     Direction::Right => {
+        //         &self.boxes[current + 1].selected = &true;
+        //     },
+        // }
+
+        // let boxik = self.boxes.get(current).unwrap();
+        &self.boxes[current] = &Box{content: BoxContent::Empty, selected: false};
+
+        // let mut boxik = &self.boxes[current];
+        // boxik.selected = false;
+        // println!("Box: {}", &self.boxes[current].selected);
+    }
+
+    pub fn print(&self) {
+        let (width, height) = terminal_size().unwrap();
+        println!("term size: {}:{}", width.0, height.0);
+        let mut output: String = String::from("");
+
+        for row in 0..3 {
+            let left_box = &self.boxes[row * 3];
+            let middle_box = &self.boxes[row * 3 + 1];
+            let right_box = &self.boxes[row * 3 + 2];
+
+            for _ in 0..((width.0 - 13) / 2) {
+                output.push_str(" ");
+            }
+
+            if left_box.selected {
+                output.push_str(format!("> {} < {} | {} |", left_box.content, middle_box.content, right_box.content).as_str());
+            }
+            else if middle_box.selected {
+                output.push_str(format!("| {} > {} < {} |", left_box.content, middle_box.content, right_box.content).as_str());
+            }
+            else if right_box.selected {
+                output.push_str(format!("| {} | {} > {} <", left_box.content, middle_box.content, right_box.content).as_str());
+            }
+            else {
+                output.push_str(format!("| {} | {} | {} |", left_box.content, middle_box.content, right_box.content).as_str());
+            }
+
+            for _ in 0..((width.0 - 13) / 2) {
+                output.push_str(" ");
+            }
+        }
+
+        print!("\r{}$ ", &output)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
+extern crate core;
 
+mod field;
+
+use std::io::{stdin, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::{IntoRawMode};
+
+fn main() {
+    let field = field::Field::new();
+
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    field.print();
+    stdout.flush().unwrap();
+
+    for c in stdin.keys() {
+        write!(
+            stdout,
+            "{}{}",
+            termion::cursor::Goto(1, 1),
+            termion::clear::All
+        ).unwrap();
+        stdout.flush().unwrap();
+
+        match c.unwrap() {
+            Key::Char('q') | Key::Ctrl('c') | Key::End | Key::Esc => {
+                stdout.flush().unwrap();
+                return;
+            },
+            Key::Char('a') | Key::Char('h') | Key::Left => field.move_selected(field::Direction::Left),
+            Key::Char('s') | Key::Char('j') | Key::Down => field.move_selected(field::Direction::Down),
+            Key::Char('w') | Key::Char('k') | Key::Up => field.move_selected(field::Direction::Up),
+            Key::Char('d') | Key::Char('l') | Key::Right => field.move_selected(field::Direction::Right),
+            _ => (),
+        }
+
+        field.print();
+        stdout.flush().unwrap();
+    }
 }
